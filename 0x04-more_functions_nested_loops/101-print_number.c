7a3909f17e9c9d57378aa3da615fc336359f5d47
Iam-kauthar <abioyekaosarat5@gmail.com>
2022-04-15 04:43:04 -0700

    102

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
-  * print_number - that prints an integer
-  * @n: print int
-  * Return: Always 0.
-  */
+* print_number - that prints an integer
+* @n: print int
+* Return: Always 0.
+*/
 void print_number(int n)
 {
 unsigned int n1;
