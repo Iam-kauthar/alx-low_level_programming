949fc6b87f454111626a5d78c0e2d5f382558b97
Iam-kauthar <abioyekaosarat5@gmail.com>
2022-04-24 00:59:50 -0700

    103

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -5,7 +5,7 @@
  * @n1: number one.
  * @n2: number two.
  * @r: buffer that the function will use to store the result.
- * @size_r: buffer size.
+ * @size_r: buffer size:
  * Return: the pointer to dest.
  */
 
