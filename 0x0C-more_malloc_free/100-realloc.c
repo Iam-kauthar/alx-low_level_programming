534ba199f226955ac751bb5054c55a8eee0fe9a6
Iam-kauthar <abioyekaosarat5@gmail.com>
2022-05-09 10:48:22 -0700

    44th

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -15,7 +15,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	unsigned int i, max = new_size;
 	char *oldp = ptr;
 
-if (ptr == NULL)
+	if (ptr == NULL)
 	{
 		p = malloc(new_size);
 		return (p);
@@ -28,7 +28,7 @@ if (ptr == NULL)
 	else if (new_size == old_size)
 		return (ptr);
 
-p = malloc(new_size);
+	p = malloc(new_size);
 	if (p == NULL)
 		return (NULL);
 	if (new_size > old_size)
@@ -38,3 +38,4 @@ p = malloc(new_size);
 	free(ptr);
 	return (p);
 }
+
